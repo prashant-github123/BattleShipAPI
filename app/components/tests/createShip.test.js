06dc6aeaabaf8fb8ef72a2bca1546d32fa63b092
Krishna Reddy <kredd3@sapient.com>
2017-06-30 14:40:26 +0530

    Test Case CreateShipJS

--- app/components/tests/createShip.test.js
+++ app/components/tests/createShip.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 // import renderer from 'react-test-renderer';
-import CreateShip from '../components/CreateShip';
+import CreateShip from '../CreateShip';
+// import {retriveShipLocation } from './../actions';
 describe('CreateShip (Shallow passing the ship coords directly)', () => {
   let wrapper;
   let output;
+  const currentUser = {
+    'message':'',
+    'status':true,
+    'statusCode':200,
+    'gameId':'',
+    'playerId':'',
+    'playerName':'',
+  };
+
   beforeEach(() => {
-    wrapper = shallow(<CreateShip output={output} />);
+    wrapper = shallow(<CreateShip currentUser={currentUser} />);
   });
   it('+++ render the DUMB component', () => {
     expect(wrapper.length).toEqual(1);
@@ -25,10 +35,24 @@ describe('CreateShip (Shallow passing the ship coords directly)', () => {
     wrapper.find('.indent').slice(1).at(1).simulate('mouseOver', { target: {
       id: '0',
     } });
+    // expect(wrapper.find('.indent').hasClass('shipSelected')).to.equal(true);
+    wrapper.find('.indent').forEach((node) => {
+      expect(node.hasClass('shipSelected')).to.equal(true);
+    });
   });
   it('on click of box change class', () => {
+    wrapper.setState({
+      rows: [0, 1, 2, 3, 4, 5, 6],
+      cols: [0, 1, 2, 3, 4, 5, 6],
+      horizontal: true,
+      shipSize: 3,
+    });
     wrapper.find('.indent').slice(1).at(1).simulate('click', { target: {
       id: '0',
     } });
+    wrapper.find('.indent').forEach((node) => {
+      expect(node.hasClass('shipSelected')).to.equal(true);
+    });
+    // expect(wrapper.find('.indent').hasClass('shipSelected')).to.equal(3);
   });
 });
